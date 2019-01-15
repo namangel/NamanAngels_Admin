19d76f71413c85f3fa316a7b235a7125e89cd8a7
akshi-saxena <38050818+akshi-saxena@users.noreply.github.com>
2019-01-15 20:49:10 +0530

    minor change

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<head>
+    <link rel="stylesheet" href="css/dashboard.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="contain">
+        <div class="side"><?php require "sidebar.php" ?></div>
+        <div class="main">
+            <div class="top a1">
+                <center>
+                    <h3>Today</h3><br>
+                    <h1>USERS</h1>
+                </center>
+            </div>
+            <div class="top a2">
+                <center>
+                    <h3>Today</h3><br>
+                    <h1>VISITED</h1>
+                </center>
+            </div>
+            <div class="top a3">
+                <center>
+                    <h3>Today</h3><br>
+                    <h1>REVENUE</h1>
+                </center>
+            </div>
+        </div>
+    </div>
+</body>
