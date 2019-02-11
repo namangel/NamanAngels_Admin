30c15329a87b8d77bd3b41158ac5856e03e4c17e
Deepali Panda <deepali4499@gmail.com>
2019-02-11 10:30:42 +0530

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="css/sidebar.css"> 
+    <link rel="stylesheet" href="css/sidebar.css">
     <style>
       * {
   padding: 0;
@@ -314,9 +314,9 @@ body {background-color: #2a2b3d;}
         </li> -->
         <li><i class="fa fa-users fa-fw"></i><a href="#"> Our team</a>
           <ul class="side-nav-dropdown">
-            <li><a href="#">View team</a></li>
+            <li><a href="team.php">View team</a></li>
             <li><a href="manage_team.php">Manage team</a></li>
-          </ul> 
+          </ul>
         </li>
         <li><i class="fa fa-address-book-o fa-fw"></i><a href="#"> View profiles</a>
           <ul class="side-nav-dropdown">
