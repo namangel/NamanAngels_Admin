67bdcf2430fdc935389e2e99ca66b8cbb2f9ccdb
readthroughmyglasses <37587359+readthroughmyglasses@users.noreply.github.com>
2019-02-11 21:34:45 +0530

    viewstartup,search remaining

---
+++ viewstartup.php
@@ -0,0 +1,265 @@
+<?php require "server.php" ?>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/dashboard.css">  -->
+<style>
+      * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: 'Droid Sans', sans-serif;
+        outline: none;
+      }
+      p{
+          color:white;
+      }
+      h1{
+          color:white;
+      }
+      .cont{
+        margin-left: 290px;
+        z-index: -100;
+      }
+      ::-webkit-scrollbar {
+        background: transparent;
+        width: 5px;
+        height: 5px;
+      }
+      ::-webkit-scrollbar-thumb {
+        background-color: #888;
+      }
+      ::-webkit-scrollbar-thumb:hover {
+        background-color: rgba(0, 0, 0, 0.3);
+      }
+      body {background-color: #2a2b3d}
+      #contents {
+        position: relative;
+        transition: .3s;
+        margin-left: 290px;
+        background-color: #2a2b3d;
+      }
+      .margin {
+        margin-left: 0 !important;
+      }
+      .welcome {
+        color: #CCC;
+      }
+      .welcome .content {
+        background-color: #313348;
+        padding: 15px;
+        margin-top: 25px;
+      }
+      .welcome h2 {
+        font-family: Calibri;
+        font-weight: 100;
+        margin-top: 0
+      }
+      .welcome p {
+        color: #999;
+      }
+
+      /* Start users */
+
+      .admins {
+        margin-top: 25px;
+      }
+      .admins .box {
+
+      }
+      .admins .box > h3 {
+        color: #ccc;
+        font-family: Calibri;
+        font-weight: 300;
+        margin-top: 0;
+      }
+      .admins .box .admin {
+        margin-bottom: 20px;
+        overflow: hidden;
+        background-color: #313348;
+        padding: 10px;
+      }
+      .admins .box .admin .img {
+        width: 25%;
+        margin-right: 5%;
+        float: left;
+      }
+      .admins .box .admin .img img {
+        border-radius: 50%;
+      }
+      .admins .box .info {
+        width: 70%;
+        color: #EEE;
+        float: left;
+      }
+      .name{
+        color:#ffc107;
+      }
+      .admins h3{
+        color: white;
+      }
+      .admins .box .info h3 {font-size: 19px}
+      .admins .box .info p {color: #BBB}
+
+      /* End users */
+      /* Start statis */
+
+      .statis {
+        color: #EEE;
+        margin-top: 15px;
+      }
+      .statis .box {
+        position: relative;
+        padding: 15px;
+        overflow: hidden;
+        border-radius: 3px;
+        margin-bottom: 25px;
+      }
+      .statis .box h3:after {
+        content: "";
+        height: 2px;
+        width: 70%;
+        margin: auto;
+        background-color: rgba(255, 255, 255, 0.12);
+        display: block;
+        margin-top: 10px;
+      }
+      .statis .box i {
+        position: absolute;
+        height: 70px;
+        width: 70px;
+        font-size: 22px;
+        padding: 15px;
+        top: -25px;
+        left: -25px;
+        background-color: rgba(255, 255, 255, 0.15);
+        line-height: 60px;
+        text-align: right;
+        border-radius: 50%;
+      }
+
+      .main-color {
+        color: #ffc107
+      }
+      .warning {background-color: #f0ad4e}
+      .danger {background-color: #d9534f}
+      .success {background-color: #5cb85c}
+      .inf {background-color: #5bc0de}
+
+
+      /* Start media query */
+
+      @media (max-width: 767px) {
+        #contents {
+          margin: 0 !important
+        }
+        .statistics .box {
+          margin-bottom: 25px !important;
+        }
+        .navbar-default .navbar-nav .open .dropdown-menu>li>a {
+          color: #CCC !important
+        }
+        .navbar-default .navbar-nav .open .dropdown-menu>li>a:hover {
+          color: #FFF !important
+        }
+        .navbar-default .navbar-toggle{
+          border:none !important;
+          color: #EEE !important;
+          font-size: 18px !important;
+        }
+        .navbar-default .navbar-toggle:focus, .navbar-default .navbar-toggle:hover {background-color: transparent !important}
+      }
+    </style>
+    <html>
+    <body>
+    <?php require "sidebar.php" ?>
+    <div class="cont">
+      <?php
+      if (isset($_GET['pageno'])) {
+        $pageno = $_GET['pageno'];
+    }
+    else {
+        $pageno = 1;
+    }
+    
+    $sname="";
+    
+    $no_of_records_per_page = 3;
+    $offset = ($pageno - 1) * $no_of_records_per_page;
+   
+    
+    
+    $total_pages_sql = "SELECT COUNT(*) FROM Profile where StpName LIKE '%{$sname}%'";
+    
+    $result = mysqli_query($db,$total_pages_sql);
+    $total_rows = mysqli_fetch_array($result)[0];
+    $total_rows = $total_rows < 1? 1:$total_rows;
+    $total_pages = ceil($total_rows/$no_of_records_per_page);
+    $sql = "SELECT * FROM Profile where StpName Like '%{$sname}%' LIMIT $offset, $no_of_records_per_page";
+             $res_data = mysqli_query($db,$sql);
+            echo '
+                  <section class="admins">
+                    <div class="container-fluid">
+                    <div class="row">
+
+                     <h3>  &nbsp;&nbsp;&nbsp;BROWSE STARTUPS</h3>
+
+
+            ';
+            while($row = mysqli_fetch_array($res_data))
+            {
+                    echo '
+                      <div class="col-md-6">
+                      <div class="box">
+                            <div class="admin">
+                            <img src='.$row['StpImg'].' alt="John" style="width:25%">
+                            <h1>'.$row['StpName'].'</h1>
+                          <p class="title">'.$row['Type'].'</p>
+                          <p>'.$row['FName'].'</p>
+                           <p>'.$row['SName'].'</p>
+                          
+
+                            </div>
+                            </div>
+                            </div>';
+            }
+            echo '
+            </div>
+            </div>
+            </section>
+            ';
+            ?>
+
+<?php
+
+
+
+            echo '<div class="pages">';
+            echo '<ul class="pagination">';
+                echo '<li><a href="?pageno=1">First</a></li>';
+                echo '<li class=';
+                    if($pageno <= 1){ echo 'disabled'; };
+                        echo '>';
+                    echo '<a href=';
+                    if($pageno <= 1){ echo "#"; } else { echo "?pageno=".($pageno - 1); };
+                    echo '>Prev</a>';
+                echo '</li>';
+                echo '<li class=';
+                    if($pageno >= $total_pages){ echo 'disabled'; };
+                    echo '>';
+                    echo '<a href=';
+                    if($pageno >= $total_pages){ echo "#"; } else { echo "?pageno=".($pageno + 1); };
+                    echo '>Next</a>';
+                echo '</li>';
+                echo "<li><a href='?pageno=$total_pages'>Last</a></li>";
+            echo '</ul>';
+        echo '</div>';
+        ?>
+      </div>
+            ?>
+    </body>
+    </html>
