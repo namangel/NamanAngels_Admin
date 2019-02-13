282248dc2ec72190a17e91faf8d982a7fc1c959b
akshi-saxena <38050818+akshi-saxena@users.noreply.github.com>
2019-02-13 10:00:59 +0530

    Update transaction.php

--- transaction.php
+++ transaction.php
@@ -1,3 +1,42 @@
+<?php 
+
+    require "server.php";
+    if(isset($_POST["transact"]))
+	{
+        $inid = mysqli_real_escape_string($db, $_POST['inv']);
+        $stupid = mysqli_real_escape_string($db, $_POST['stup']);
+        $status = mysqli_real_escape_string($db, $_POST['stat']); 
+
+		if ($status == 'done')
+		{
+            $q = "UPDATE requests SET Deal = 1 WHERE St_ID='$stupid' AND Inv_ID='$inid';";
+			mysqli_query($db, $q);
+		}
+        
+        if ($status == 'interest')
+		{
+            $q1 = "SELECT * FROM requests WHERE St_ID='$stupid' AND Inv_ID='$inid';";
+            $res1 = mysqli_query($db, $q1);
+            $row = mysqli_fetch_assoc($res1);
+            if ($row['Deal'] == 1)
+            {
+                echo "<script>alert('Deal Already Done')</script>";
+            }
+            else{
+            $q = "INSERT into requests(Inv_ID,St_ID) values ('$inid','$stupid')";
+            mysqli_query($db, $q);
+            }
+		}
+        
+        if ($status == 'cancel')
+		{
+            $q = "DELETE FROM requests WHERE St_ID='$stupid' AND Inv_ID='$inid'";
+			mysqli_query($db, $q);
+		}
+		header('location: transaction.php');
+	}
+
+?>
 <html>
     <head>
         <style>
@@ -46,14 +85,34 @@
         <div class="cont">
             <div class="tab">    
                 <center>
-                <form>
-                    <label>Startup Name: </label><input type="text">
-                    <label>Investor Name: </label><input type="text">
-                    <select>
-                        <option>Investor Interested</option>
-                        <option>Deal Complete</option>
+                <form method="post" action="transaction.php">
+                    <label>Startup Name: </label>
+                    <select name="stup">
+                        <?php
+                            $qu1= "SELECT * FROM st_details;";
+                            $res1 = mysqli_query($db, $qu1);
+                            while($row1 = mysqli_fetch_assoc($res1)){
+                                echo '<option value="'.$row1['StpID'].'">'.$row1['Stname'].'</option>'; 
+                            }
+                        ?>   
                     </select>
-                    <input type="submit" value="Update" class="butn">
+                    <label>Investor Name: </label>
+                    <select name="inv">
+                        <?php
+                            $qu1= "SELECT * FROM inv_details;";
+                            $res1 = mysqli_query($db, $qu1);
+                            while($row1 = mysqli_fetch_assoc($res1)){
+                                echo '<option value="'.$row1['InvID'].'">'.$row1['CName'].'</option>'; 
+                            }
+                        ?>   
+                    </select>
+                    <label>Status: </label>
+                    <select name="stat">
+                        <option value="interest">Investor Interested</option>
+                        <option value="done">Deal Complete</option>
+                        <option value="cancel">Deal Canceled</option>
+                    </select>
+                    <input type="submit" name="transact" value="Update" class="butn">
                 </form>
                 <table>
                     <tr>
@@ -61,16 +120,35 @@
                         <th>Startup</th>
                         <th>Status</th>
                     </tr>
-                    <tr>
-                    <td>ABC</td>
-                    <td>PQR</td>
-                    <td>Investor Interested</td>
-                    </tr>
-                    <tr>
-                    <td>XYZ</td>
-                    <td>LMN</td>
-                    <td>Deal Complete</td>
-                    </tr>
+                    <?php 
+                    $qu = "SELECT * FROM requests";
+                    $results = mysqli_query($db, $qu);
+                    while($row = mysqli_fetch_assoc($results))
+                    {
+                        $deal = $row['Deal'];
+                        $stpid = $row['St_ID'];
+                        $invid = $row['Inv_ID'];
+                        
+                        $qu1= "SELECT * FROM st_details WHERE StpID = '$stpid'";
+                        $res1 = mysqli_query($db, $qu1);
+                        $row1 = mysqli_fetch_assoc($res1);
+
+                        $qu2= "SELECT * FROM inv_details WHERE InvID = '$invid'";
+                        $res2 = mysqli_query($db, $qu2);
+                        $row2 = mysqli_fetch_assoc($res2);
+                        
+                        echo '<tr>
+                        <td>'.$row1['Stname'].'</td>
+                        <td>'.$row2['CName'].'</td>'
+                        ;
+                        if($deal == 0)
+                            echo '<td>Investor Interested</td>
+                                </tr>'; 
+                        if($deal == 1)
+                            echo '<td>Deal Completed</td>
+                                </tr>'; 
+                    }
+                    ?>
                 </table>
                 </center>
             </div>
